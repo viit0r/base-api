61c4bb341cd8a61b69a8ab265a777f3525050ea2
viit0r <vituuoliveira1@gmail.com>
2023-06-02 16:25:40 -0300

    Criação do projeto

---
+++ src/test/java/com/viit0r/baseapi/BaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viit0r.baseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
